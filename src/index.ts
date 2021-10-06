b5434ed50d5f412ab296c985cf9416e8cf69f54e
Ralph Effting <ralfting@gmail.com>
2021-10-06 17:30:19 -0300

    Get posts by /feed and get post by id in /post/:id

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,26 @@ app.use(express.json())
 app.get('/users', async (_req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
-})
+});
+
+app.get('/feed', async (_req, res) => {
+  const posts = await prisma.posts.findMany({
+    where: { published: true },
+    include: { author: true }
+  });
+
+  res.json(posts);
+});
+
+app.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.posts.findFirst({
+    where: { id: parseInt(id) },
+    include: { author: true }
+  });
+
+  res.json(post);
+});
 
 app.listen(3000, () =>
   console.log('REST API server ready at: http://localhost:3000'),
