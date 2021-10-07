51691d28a44fe720b6271a33ff7ebe98a8826db1
Ralph Effting <ralfting@gmail.com>
2021-10-07 08:31:44 -0300

    Add routes to create a user and post, publish and delelete post

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,13 @@ app.get('/users', async (_req, res) => {
   res.json(users);
 });
 
+app.post('/users', async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: { ...req.body }
+  });
+  res.json(newUser);
+})
+
 app.get('/feed', async (_req, res) => {
   const posts = await prisma.posts.findMany({
     where: { published: true },
@@ -21,14 +28,49 @@ app.get('/feed', async (_req, res) => {
   res.json(posts);
 });
 
-app.get('/post/:id', async (req, res) => {
+app.get('/posts/:id', async (req, res) => {
   const { id } = req.params;
   const post = await prisma.posts.findFirst({
-    where: { id: parseInt(id) },
+    where: { id: Number(id) },
     include: { author: true }
   });
 
   res.json(post);
+});
+
+app.post('/posts', async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const newPost = await prisma.posts.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } }
+    }
+  });
+
+  res.json(newPost);
+});
+
+app.put('/posts/publish/:id', async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.posts.update({
+    where: { id: Number(id)},
+    data: {
+      published: true
+    }
+  });
+
+  res.json(post);
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const postDeleted = await prisma.posts.delete({
+    where: { id: Number(id) }
+  });
+
+  res.json(postDeleted);
 });
 
 app.listen(3000, () =>
